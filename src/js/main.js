517aa026d974291941df90d70a5bfa5f136ac48f
Piotr Miszczak <miszpiotr@wp.pl>
2020-05-27 22:47:33 +0200

    feat:corrected paddings and JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,33 @@
 "use strict";
 // place your code below
-
-
-
+const wrapper = document.querySelector('.projects__wrapper')
+fetch('https://api.github.com/users/PiotrMiszczak/repos')
+.then(resp => resp.json())
+.then(resp =>{
+    const repos = resp 
+    for(const repo of repos){
+       if(repo.name=='PiotrMiszczak'){
+        wrapper.innerHTML += `<div class="projects__item">
+        <div class="projects__item--content">
+        <img src="assets/img/Vector.svg">
+        <h3 class="projects__item--header">Portfolio</h3>
+        <p>Brak opisu</p>
+      </div>
+        <footer class="projects__footer">
+         <a href="${repo.html_url}"><img src="assets/img/Git icon.svg"></a><a class="projects__footer--link" href="${repo.html_url}">Github</a>
+        </footer>
+      </div>`}
+      else{
+      wrapper.innerHTML += `<div class="projects__item">
+      <div class="projects__item--content">
+      <img src="assets/img/Vector.svg">
+      <h3 class="projects__item--header">${repo.name}</h3>
+      <p>${repo.description}</p>
+    </div>
+      <footer class="projects__footer">
+       <a href="https://piotrmiszczak.github.io/${repo.name}/"><img src="assets/img/Demo Icon.svg"></a><a class="projects__footer--link projects__footer--link-demo" href="https://piotrmiszczak.github.io/${repo.name}/">Demo</a>
+       <a href="${repo.html_url}"><img src="assets/img/Git icon.svg"></a><a class="projects__footer--link" href="${repo.html_url}">Github</a>
+      </footer>
+    </div>`}}})
+    
 
