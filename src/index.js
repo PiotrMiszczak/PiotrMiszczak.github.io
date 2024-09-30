1b38d4df9451f3b4f7cfa3851364dc86f542755c
Piotr Miszczak <miszpiotr@wp.pl>
2024-09-30 21:48:32 +0200

    fixed bug with horizontal scroll

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 window.onload=function(){
 const hamburgerButton = document.getElementById("hamburgerMenu");
 const modalMenu = document.getElementById("modalMenu");
-console.log("jestem");
 hamburgerButton.addEventListener("click",toggleMenu)
 
 
 }
 
 function toggleMenu(){
-    console.log("tu, ludzi tlum");
+  
     modalMenu.classList.toggle("menu-active");
-    this.classList.toggle("button-active")
+    this.classList.toggle("button-active");
+    document.body.classList.toggle("stop-scroll");
+   
 }
